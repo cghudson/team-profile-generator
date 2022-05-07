c9102671fe896421c34cfe45610c627c3d432bcd
Carolyn Hudson <96785153+cghudson@users.noreply.github.com>
2022-05-06 22:13:50 -0500

    add manager prompts

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
-const inquirer =  require('inquirer');
-const path = require('path');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
-const MAIN_DIR = path.resolve(__dirname, 'dist');
-const filePath = path.join(MAIN_DIR, 'teamProfile.html');
+const MAIN_DIR = path.resolve(__dirname, "dist");
+const filePath = path.join(MAIN_DIR, "teamProfile.html");
 
-const renderHtml = require('./src/generateHTML');
-const Manager = require('./lib/Manager');
+const renderHtml = require("./src/generateHTML");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
 
 const teamArray = [];
 
-function start(){
-    function createManager(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'managerName',
-                message: "What is the Manager's name?"
-            }
-        ]).then((res)=> {
-            const manager = new Manager(
-                res.managerName
-            )
-            teamArray.push(manager)
-            menu()
-        })
-    }
-
-    function menu(){
-        inquirer.prompt([
+function start() {
+  function createManager() {
+    inquirer
+      .prompt([
         {
-            type: 'list',
-            name: 'userChoice',
-            message: 'What would you like to do?', 
-            choices: ['Add an Engineer', 'Add an Intern', 'Create my HTML']
-        }
-    ]).then(res  => {
+          type: "input",
+          name: "managerName",
+          message: "What is the Manager's name?",
+        },
+        {
+          type: "input",
+          name: "managerID",
+          message: "What is the Manager's ID number?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the Manager's email address?",
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the Manager's office number?",
+        },
+      ])
+      .then((res) => {
+        const manager = new Manager(res.managerName, res.managerID, res.email, res.officeNumber);
+        teamArray.push(manager);
+        menu();
+      });
+  }
 
-    })
-    }
+  function menu() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "userChoice",
+          message: "What would you like to do?",
+          choices: ["Add an Engineer", "Add an Intern", "Create my HTML"],
+        },
+      ])
+      .then((res) => {});
+  }
 
-
-
-    // inquirer.prompt([
-    //     {
-    //         type: 'input',
-    //         name: 'fullName',
-    //         message: 'what is your full name?'
-    //     }
-    // ]).then(res =>{
-    //     if(!fs.existsSync(MAIN_DIR)){
-    //         fs.mkdirSync(MAIN_DIR)
-    //     }
-    //     fs.writeFileSync(filePath, renderHtml(), 'utf-8')
-    // })
+  // inquirer.prompt([
+  //     {
+  //         type: 'input',
+  //         name: 'fullName',
+  //         message: 'what is your full name?'
+  //     }
+  // ]).then(res =>{
+  //     if(!fs.existsSync(MAIN_DIR)){
+  //         fs.mkdirSync(MAIN_DIR)
+  //     }
+  //     fs.writeFileSync(filePath, renderHtml(), 'utf-8')
+  // })
 }
-start()
+start();
